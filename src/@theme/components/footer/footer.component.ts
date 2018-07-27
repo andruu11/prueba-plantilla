936e2cd57eec22a871d49675d3d780d8dbaecc72
Jhassir Candia <andruu11.jhass@gmail.com>
2018-07-26 23:27:45 -0400

    V1

--- src/@theme/components/footer/footer.component.ts
+++ src/@theme/components/footer/footer.component.ts
@@ -4,12 +4,13 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">Created with ♥ by <b><a href="https://akveo.com" target="_blank">Akveo</a></b> 2017</span>
+    <span class="created-by">Modified by <b><a href="https://akveo.com" target="_blank">PushUp</a></b> 2018</span>
     <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-github"></a>
-      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
-      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
-      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
+      <a href="https://www.instragram.com" target="_blank" class="ion ion-social-instagram"></a>
+      <a href="https://www.whatsapp.com" target="_blank" class="ion ion-social-whatsapp"></a>
+      <a href="https://www.facebook.com/Push-Up-160295974649289/" target="_blank" class="ion ion-social-facebook"></a>
+      <a href="https://www.twitter.com" target="_blank" class="ion ion-social-twitter"></a>
+      <a href="https://www.linkedin.com" target="_blank" class="ion ion-social-linkedin"></a>
     </div>
   `,
 })
